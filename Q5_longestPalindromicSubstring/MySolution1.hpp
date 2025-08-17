928c3b49467aeb6b8a5a29b2dae95e37192c141d
WongAndyStarSB <sbandystar@gmail.com>
2025-08-17 14:01:42 +0800

    Update MySolution1.hpp

--- Q5_longestPalindromicSubstring/MySolution1.hpp
+++ Q5_longestPalindromicSubstring/MySolution1.hpp
@@ -16,9 +16,31 @@ using namespace std;
 
 class Solution {
 public:
+    class Solution {
+public:
     string longestPalindrome(string s) {
-
+        int size = s.size() ;
+        for (int l = size; l > 0; l--) {
+            for (int i = 0; i <= size-l; ++i) {
+                bool is_pldmic = true;
+                for (int j = 0; j < l/2; ++j) {
+                    if (s[i+j] != s[i+l-j-1] ) {
+                        is_pldmic = false;
+                        break;
+                    }
+                }
+                if (is_pldmic) {
+                    std::string pld_s;
+                    for (int j = i; j < i+l; ++j) {
+                        pld_s += s[j] ;
+                    }
+                    return pld_s;
+                }
+            }
+        }
+        throw;
     }
+};
 };
 
 #endif // Q5_SOLUTION_HPP
